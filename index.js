64cef874f7a9e61530332646ae7d2819571d21e7
falencarlima <falencarlima@gmail.com>
2023-09-21 00:02:32 -0400

    recebe entrada de dados do usuário e utiliza estrutura if else para classificar Herói

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-print("Desafio do Felipão")
+var readline = require('readline-sync')
+
+let nomeHeroi = readline.question("Digite o nome do Heroi: ")
+let xpHeroi = Number(readline.question("Qual o XP do Heroi?: "))
+let nivelHeroi
+
+if(xpHeroi <= 1000){
+    nivelHeroi = "Ferro"
+}
+else if(xpHeroi > 1000 && xpHeroi <= 2000){
+    nivelHeroi = "Bronze"
+}
+else if(xpHeroi > 2000 && xpHeroi <= 5999){
+    nivelHeroi = "Prata"
+}
+else if(xpHeroi > 6000 && xpHeroi <= 7000){
+    nivelHeroi = "Ouro"
+}
+else if(xpHeroi > 8000 && xpHeroi <= 9000){
+    nivelHeroi = "Platina"
+}
+else if(xpHeroi > 9000 && xpHeroi <= 10000){
+    nivelHeroi = "Imortal"
+}
+else{
+    nivelHeroi = "Radiante"
+}
+
+console.log("O Herói " + nomeHeroi + " está no nível de " + nivelHeroi)
